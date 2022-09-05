30e97df0cbc99d0cb32bc9c381b55661c16d7f14
michaelisaiah02 <michael.isaiah.02@gmail.com>
2022-09-06 02:07:06 +0700

    mengubah sistem contact-form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+const form = document.forms["contact-form"];
 const contactForm = document.getElementById("contact-form");
-const loader = document.querySelector(".loader");
+const btnLoading = document.querySelector(".btn-loading");
+const btnKirim = document.querySelector(".btn-kirim");
+const alertBerhasil = document.querySelector(".alert-berhasil");
 
-loader.style.display = "none";
+// btnLoading.style.display = 'none';
 
 contactForm.addEventListener("submit", function (e) {
-  loader.style.display = "block";
   e.preventDefault();
+  // tampilkan tombol loading, hilangkan tombol kirim
+  // btnLoading.style.display = "block";
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
 
   const url = e.target.action;
   const formData = new FormData(contactForm);
@@ -15,10 +21,17 @@ contactForm.addEventListener("submit", function (e) {
     body: formData,
     mode: "no-cors",
   })
-    .then(() => {
-      //url terima kasih
-      loader.style.display = "none";
-      window.location.href = "terimakasih.html";
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      alertBerhasil.classList.toggle("d-none");
+      // reset formnya
+      contactForm.reset();
+      // btnLoading.style.display = "none";
+      // window.location.href = "terimakasih.html";
+      console.log("Success!", response);
     })
     .catch((e) => alert("Error occured"));
 });
